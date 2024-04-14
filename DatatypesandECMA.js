c77198336cc8fbe1b1c53c004a96ce21e14a04a5
moonlight2909 <167037596+moonlight2909@users.noreply.github.com>
2024-04-15 05:03:34 +0530

    Add files via upload

---
+++ DatatypesandECMA.js
@@ -0,0 +1,4 @@
+"use strict" // treat all code as a new version of js       
+let name = "Himanshu";
+let age = 18;
+let account_Status = true;
---
+++ varandcons.js
@@ -0,0 +1,7 @@
+const account_Id = 12104452;
+let Email_Address = "Himanshukashyap@gmail.com";
+var account_Password = "12345";
+Account_City = "Jaipur";
+let account_number;
+console.table([account_Id ,account_Password,account_number,Account_City,Email_Address]);
+
