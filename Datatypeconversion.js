05042d29412a9d8e859280a8918e5486383879e5
Himanshu <167037596+moonlight2909@users.noreply.github.com>
2024-04-15 17:00:53 +0530

    Add files via upload

---
+++ Datatypeconversion.js
@@ -0,0 +1,15 @@
+let score = "himanshu";
+console.log(typeof(score));
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+// "33" => 33
+//"33abc" => NaN
+// true => 1 ; false=> 0
+
+let isloggedIn = "himanshu";
+let boolenisloggedIn = Boolean(isloggedIn);
+console.log(typeof(boolenisloggedIn));
---
+++ operation.js
@@ -0,0 +1,23 @@
+let value = 3;
+let negvalue = -value;
+console.log(negvalue);
+
+
+let str1 = "hello";
+let str2 = "himanshu";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log("1" + 2 + 2);
+console.log(2 + "1");
+console.log(2 + 2 + "1");
+
+console.log(+true);
+console.log(+"");
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+
+let gamecounter = 100;
+gamecounter++;
+console.log(gamecounter);
