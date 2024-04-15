9b2c53ffd9325c0e3f5c84816f20af04533586c0
Himanshu <167037596+moonlight2909@users.noreply.github.com>
2024-04-15 17:12:08 +0530

    Add files via upload

---
+++ Datatypesofjavascript.js
@@ -0,0 +1,33 @@
+// Primitive
+
+// 7 Types
+//string
+//Number
+//Boolean
+//Null
+//BigInt
+//undefined
+//Symbols
+
+//reference (Non primitive)
+// Array , Object , Function
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outSidetemp = NULL;
+
+let useEmail;
+const id = Symbol('123');
+const anotherId = Symbol('123');
+console.log(id==anotherId);
+
+const hero = ["himanshu" , "raman"];
+let myObj = {
+    name: "himanshu",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Himanshu");
+}
