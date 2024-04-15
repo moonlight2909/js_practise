e25402191a43ae6bda42bc73bae85e0908c05187
Himanshu <167037596+moonlight2909@users.noreply.github.com>
2024-04-15 17:22:42 +0530

    Add files via upload

---
+++ stackandheao.js
@@ -0,0 +1,16 @@
+// stack(Primitive) , head(NON - primitive);
+let myName = "HIMANSHU";
+let anotherName = myName;
+anotherName="kashyap";
+console.log(myName)
+console.log(anotherName);
+
+let user ={
+    email: "user@gmail.com",
+    upi: "user2ybl"
+}
+
+let userTwo = userone;
+userTwo.email = "himanshukashyap@gmail.com";
+console.log(userone.email);
+console.log(userTwo.email);
