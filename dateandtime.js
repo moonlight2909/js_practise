1e2ba513747b448e81eb2297258186c559b158db
Himanshu <167037596+moonlight2909@users.noreply.github.com>
2024-04-18 13:53:17 +0530

    Add files via upload

---
+++ dateandtime.js
@@ -0,0 +1,14 @@
+// dates
+let myDates = new Date();
+console.log(myDates.toString());
+console.log(myDates.toUTCString);
+console.log(typeof myDates);
+
+let mycreatedate = new Date(2024, 0, 24);
+console.log(mycreatedate.toDateString());
+
+let mytimeStamp = Date.now();
+// console.log(mytimeStamp);
+// console.log(mytimeStamp.getTime());
+console.log(math.floor(mytimeStamp.now() / 1000));
+
