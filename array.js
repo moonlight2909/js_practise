8d91aaf0b0a00277ef8e95222204eb138598778e
Himanshu <167037596+moonlight2909@users.noreply.github.com>
2024-04-18 15:06:05 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,14 @@
+// array
+let nmyarr = [0, 1, 2, 3, 4, 5];
+console.log(myarr[2]);
+
+const myarr = new Array(1, 2, 3, 4);
+console.log(myarr[3]);
+
+//array method
+myarr.push(6);
+console.log(myarr);
+myarr.unshift(0);
+// slice,splice
+const myn1 = myarr.slice(1,3);
+console.log(myn1);
