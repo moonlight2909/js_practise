e0abe39b8934e12a825d6e29ab3f622e83961cce
Himanshu <167037596+moonlight2909@users.noreply.github.com>
2024-04-23 22:48:42 +0530

    Add files via upload

---
+++ funstion.js
@@ -0,0 +1,29 @@
+// console.log("h");
+// console.log("i");
+// console.log("m");
+// console.log("a");
+// console.log("n");
+// console.log("s");
+// console.log("h");
+// console.log("u");
+
+function saymyname() {
+    console.log("h");
+    console.log("i");
+    console.log("m");
+    console.log("a");
+    console.log("n");
+    console.log("s");
+    console.log("h");
+    console.log("u");
+}
+// saymyname();
+function addtwonumber(number1 ,number2){
+    console.log(number1+number2);
+}
+addtwonumber(3,5);
+
+function loginuserMessage(username){
+    return `${username} just logged in`
+}
+console.log(loginuserMessage("himanshu"));
