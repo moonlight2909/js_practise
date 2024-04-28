be8cfeebb3039566e84f756796abeb9211cf3808
Himanshu <167037596+moonlight2909@users.noreply.github.com>
2024-04-28 20:42:58 +0530

    Add files via upload

---
+++ foreachloop.js
@@ -0,0 +1,7 @@
+const arr = [1,2,3,4,5,6,7,8,9,10];
+arr.forEach( function (item) {
+    console.log(item);
+})
+arr.forEach((item)=>{
+    console.log(item);
+})
---
+++ invokedfunctioniife.js
@@ -0,0 +1,4 @@
+function one(){
+    console.log(`DB CONNECTED`);
+}
+one();
